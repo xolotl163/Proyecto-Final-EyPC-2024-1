b2ab34fbc01f264f84c71eef7295172d9ef3f6fe
xolotl163 <xolotl.ximenez163@gmail.com>
2023-11-16 19:42:15 -0600

    se ha empezado a crear el codigo fuente del archivo main y se crearon los objetos de registros, banderas e instrucciones

---
+++ main.py
@@ -0,0 +1,8 @@
+
+import dataStructures.generalUse1 as generalUses
+
+#sección de creacion e inicialización de variables globales
+
+flags = generalUses.Flags
+registers = generalUses.Registers
+instructions = generalUses.Instructions
