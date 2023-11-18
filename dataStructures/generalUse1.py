68d00031b6a9b0a82ac189757a9ef637e10469bf
xolotl163 <xolotl.ximenez163@gmail.com>
2023-11-18 13:51:37 -0600

    actualización de estrucutras de datos

--- dataStructures/generalUse1.py
+++ dataStructures/generalUse1.py
@@ -6,32 +6,39 @@ class Dictionary:
     #constructor
     def __init__( self, name ):
         self.name = name
-        self.dictionary = {}
+        self.mainDict = {}
 
     #setters and getters
     def setName(self, newName):
         self.name = newName
     
-    def setDictioanry(self, newDictionary):
+    def setMainDict(self, newDictionary):
         self.dictionary = newDictionary
+
+    def setDictionary(self, dictionary, newDictionary):
+        self.getMainDict()[dictionary]
 
     def getName(self):
         return self.name
 
-    def getDictionary(self):
-        return self.dictionary
+    def getMainDict(self):
+        return self.mainDict
+
+    def getDictionary(self, nameDict):
+        return self.getMainDict()[ nameDict ]
+
+    def getElement(self, dictionary, key):
+        return self.getDictionary()[ dictionary ][ key ]
 
     #general methods
+    def addDictionary(self, nameDictionary):
+        self.getMainDict()[nameDictionary] = {}
 
-    def addElement(self, key, newElement):
-        self.getDictionary()[ key ] = newElement
-
-    def getElement(self, key):
-        return self.getDictionary()[ key ]
+    def addElement(self, dictionary, key, newElement):
+        self.getMainDict()[ dictionary ][ key ] = newElement
     
-    def editElement(self, key, newElement):
-        self.getDictionary()[ key ] = newElement
-
+    def editElement(self, dictionary, key, newElement):
+        self.getMainDict()[ dictionary ][ key ] = newElement
 
 class Stack:
 
@@ -72,53 +79,69 @@ class Stack:
 
 """ creación de las estructas y los datos """
 
+#instanciación de los objetos
 Flags = Dictionary("flags")
-Flags.addElement("CF",  False)
-Flags.addElement("OF",  False)
-Flags.addElement("ZF",  False)
-Flags.addElement("EF",  False)
-Flags.addElement("GF",  False)
-Flags.addElement("SMF", False)
-
 Registers = Dictionary("registers")
-Registers.addElement("r1",  "0000")
-Registers.addElement("r2",  "0001")
-Registers.addElement("r3",  "0010")
-Registers.addElement("r4",  "0011")
-Registers.addElement("r5",  "0100")
-Registers.addElement("r6",  "0101")
-Registers.addElement("rR",  "0110")
-Registers.addElement("rW",  "0111")
-Registers.addElement("rSA", "1000")
-Registers.addElement("rSF", "1001")
-
 Instructions = Dictionary("instructions")
-Instructions.addElement("add",  "000000")
-Instructions.addElement("sub",  "000001")
-Instructions.addElement("mul",  "000010")
-Instructions.addElement("cmp",  "000011")
-Instructions.addElement("divC", "000100")
-Instructions.addElement("divR", "000101")
-Instructions.addElement("and",  "000110")
-Instructions.addElement("or",   "000111")
-Instructions.addElement("xor",  "001000")
-Instructions.addElement("shl",  "001001")
-Instructions.addElement("shr",  "001010")
-Instructions.addElement("rol",  "001011")
-Instructions.addElement("ror",  "001100")
-Instructions.addElement("test", "001101")
-Instructions.addElement("mov",  "001110")
-Instructions.addElement("wrt",  "001111")
-Instructions.addElement("read", "010000")
-Instructions.addElement("call", "010001")
-Instructions.addElement("puAS", "010010")
-Instructions.addElement("popSF","010011")
-Instructions.addElement("popAS","010100")
-Instructions.addElement("jmp",  "010101")
-Instructions.addElement("je",   "010110")
-Instructions.addElement("jne",  "010111")
-Instructions.addElement("ja",   "011000")
-Instructions.addElement("jae",  "011001")
-Instructions.addElement("jb",   "011010")
-Instructions.addElement("jbe",  "011011")
-Instructions.addElement("not",  "011100")
+
+#creación de los diccionarios
+Flags.addDictionary("bool")
+Flags.addDictionary("binary")
+Registers.addDictionary("binary")
+Instructions.addDictionary("binary")
+
+#se añaden los datos
+Flags.addElement( "bool", "CF",  False)
+Flags.addElement( "bool", "OF",  False)
+Flags.addElement( "bool", "ZF",  False)
+Flags.addElement( "bool", "EF",  False)
+Flags.addElement( "bool", "GF",  False)
+Flags.addElement( "bool", "SMF", False)
+
+Flags.addElement( "binary", "CF",  "0000")
+Flags.addElement( "binary", "OF",  "0001")
+Flags.addElement( "binary", "ZF",  "0010")
+Flags.addElement( "binary", "EF",  "0011")
+Flags.addElement( "binary", "GF",  "0100")
+Flags.addElement( "binary", "SMF", "0101")
+
+Registers.addElement( "binary", "r1",  "0000")
+Registers.addElement( "binary", "r2",  "0001")
+Registers.addElement( "binary", "r3",  "0010")
+Registers.addElement( "binary", "r4",  "0011")
+Registers.addElement( "binary", "r5",  "0100")
+Registers.addElement( "binary", "r6",  "0101")
+Registers.addElement( "binary", "rR",  "0110")
+Registers.addElement( "binary", "rW",  "0111")
+Registers.addElement( "binary", "rSA", "1000")
+Registers.addElement( "binary", "rSF", "1001")
+
+Instructions.addElement( "binary", "add",  "000000")
+Instructions.addElement( "binary", "sub",  "000001")
+Instructions.addElement( "binary", "mul",  "000010")
+Instructions.addElement( "binary", "cmp",  "000011")
+Instructions.addElement( "binary", "divC", "000100")
+Instructions.addElement( "binary", "divR", "000101")
+Instructions.addElement( "binary", "and",  "000110")
+Instructions.addElement( "binary", "or",   "000111")
+Instructions.addElement( "binary", "xor",  "001000")
+Instructions.addElement( "binary", "shl",  "001001")
+Instructions.addElement( "binary", "shr",  "001010")
+Instructions.addElement( "binary", "rol",  "001011")
+Instructions.addElement( "binary", "ror",  "001100")
+Instructions.addElement( "binary", "test", "001101")
+Instructions.addElement( "binary", "mov",  "001110")
+Instructions.addElement( "binary", "wrt",  "001111")
+Instructions.addElement( "binary", "read", "010000")
+Instructions.addElement( "binary", "call", "010001")
+Instructions.addElement( "binary", "puAS", "010010")
+Instructions.addElement( "binary", "popSF","010011")
+Instructions.addElement( "binary", "popAS","010100")
+Instructions.addElement( "binary", "jmp",  "010101")
+Instructions.addElement( "binary", "je",   "010110")
+Instructions.addElement( "binary", "jne",  "010111")
+Instructions.addElement( "binary", "ja",   "011000")
+Instructions.addElement( "binary", "jae",  "011001")
+Instructions.addElement( "binary", "jb",   "011010")
+Instructions.addElement( "binary", "jbe",  "011011")
+Instructions.addElement( "binary", "not",  "011100")
